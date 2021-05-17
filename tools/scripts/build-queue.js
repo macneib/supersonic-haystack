392a8bbca572829e5065cd9ee9816f257ceee7fb
Blair MacNeil <blair.macneil@enverus.com>
2021-05-17 08:31:42 -0600

    new and improved queue

--- tools/scripts/build-queue.js
+++ tools/scripts/build-queue.js
@@ -4,26 +4,7 @@ const isMaster = process.argv[2] === 'False';
 const baseSha = isMaster ? 'origin/master~1' : 'origin/master';
 const  shell = require('shelljs');
 shell.config.silent = true;
-//  get branch
-// const branch = execSync(`git rev-parse --abbrev-ref HEAD`).toString()
-// console.log(`selected branch ${branch}`)
 
-// // Retrieve the modified files, excluding the merge commit
-// const merge_commit_hash= execSync(`git rev-parse --short HEAD`).toString()
-// console.log(merge_commit_hash)
-// const build_commit_hash= execSync(`git rev-list --no-merges -n1 HEAD`).toString()
-// console.log(build_commit_hash)
-
-// const files = JSON.stringify(
-//   execSync(`git diff-tree --no-commit-id --name-only -r ${build_commit_hash}`)
-// .toString()
-// .trim()
-// ).map
-// console.log(files)
-// // // console.log(files)
-// // for (var i = 0; i < files.length; i++) {
-// //   [i])
-// // }
 
 let affected = shell.exec('./tools/scripts/build_queue.sh')
 let affectedToBuild = affected.stdout.replace(/^\s+|\s+$/g, '').split(" ");
@@ -31,31 +12,3 @@ let affectedToBuild = affected.stdout.replace(/^\s+|\s+$/g, '').split(" ");
 for (var i = 0; i < affectedToBuild.length; i++) {
   console.log(affectedToBuild[i])
 }
-
-// let affected = child.exec('sh ./tools/scripts/build_queue.sh', (error, stdout, stderr) => {
-//   console.log(stdout);
-//   console.log(stderr);
-//   if (error !== null) {
-//       console.log(`exec error: ${error}`);
-//   }
-// });
-
-
-function commands(target) {
-    const array = JSON.parse(
-      execSync(`npx nx print-affected --base=${baseSha} --target=${target}`)
-        .toString()
-        .trim()
-    ).tasks.map(t => t.target.project);
-  
-    array.sort(() => 0.5 - Math.random());
-    const third = Math.floor(array.length / 3);
-    const a1 = array.slice(0, third);
-    const a2 = array.slice(third, third * 2);
-    const a3 = array.slice(third * 2);
-    return {
-      [target + '1']: a1,
-      [target + '2']: a2,
-      [target + '3']: a3
-    };
-  }
